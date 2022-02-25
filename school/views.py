6d463e33f1a34df8b82ff2fe3134ee276e6fe0aa
yokwejuste <yokwejuste@yahoo.com>
2022-02-25 11:04:41 +0100

    updates

--- school/views.py
+++ school/views.py
@@ -6,11 +6,11 @@ config = {
     "apiKey": env("F_API"),
     "authDomain": env("F_AUTH_DOMAIN"),
     "databaseURL": env("F_DATABASE_URL"),
-    "projectId": env("doclms"),
-    "storageBucket": env("doclms.appspot.com"),
+    "projectId": env("F_PROJECT_ID"),
+    "storageBucket": env("F_STORAGE_BUCKET"),
     "messagingSenderId": env("F_MESSAGING_SENDER_ID"),
     "appId": env("F_APP_ID"),
-    "measurementId": env("G-TC8W87WMRT")
+    "measurementId": env("F_MEASUREMENT_ID")
 }
 firebase = pyrebase.initialize_app(config)
 authe = firebase.auth()
