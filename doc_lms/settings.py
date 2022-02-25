afb9b4d06149f040e24164014daf911a0a28eec5
yokwejuste <yokwejuste@yahoo.com>
2022-02-25 10:45:14 +0100

    created new app

--- doc_lms/settings.py
+++ doc_lms/settings.py
@@ -11,10 +11,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(y)b+8$mgt$*(f64ow9tdijida&e&l-0ur3$dkdmy4cfh8n-%3'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -66,12 +66,12 @@ WSGI_APPLICATION = 'doc_lms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- doc_lms/urls.py
+++ doc_lms/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-
-from .views import *
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include("school.views")),
 ]
