52da3fac6b8ae33e99b4993bd5a166f8dbc6d642
yokwejuste <yokwejuste@yahoo.com>
2022-03-06 18:40:18 +0100

    updates

--- school/views.py
+++ school/views.py
@@ -5,13 +5,13 @@ from .pyrebase_settings import *
 
 def index(request):
     # accessing our firebase data and storing it in a variable
-    name = database.child('Data').child('Name').get().val()
-    stack = database.child('Data').child('Stack').get().val()
-    framework = database.child('Data').child('Framework').get().val()
+    # name = database.child('Data').child('Name').get().val()
+    # stack = database.child('Data').child('Stack').get().val()
+    # framework = database.child('Data').child('Framework').get().val()
 
     context = {
-        'name': name,
-        'stack': stack,
-        'framework': framework
+        # 'name': name,
+        # 'stack': stack,
+        # 'framework': framework
     }
     return render(request, 'student/index.html', context)
