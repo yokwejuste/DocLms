b884adfbf201d1caaa6babbc21d6fc29fc0441ee
yokwejuste <yokwejuste@yahoo.com>
2022-03-11 16:25:02 +0100

    update

--- school/urls.py
+++ school/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('about', views.about),
+    path('contact', views.contact),
+    path('teacher', views.teachers),
+    path('student', views.dashboard),
+    path('contact', views.contact),
+    path('blog', views.blog),
+    path('course', views.courses),
+    path('course-single', views.single_course),
+    path('blog-single', views.single_blog),
+    path('shop', views.shop),
 ]
