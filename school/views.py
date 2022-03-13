d4d415137ba0a2ca9b0de4dc9a23332c7019e72c
yokwejuste <yokwejuste@yahoo.com>
2022-03-13 23:56:33 +0100

    update

--- school/views.py
+++ school/views.py
@@ -134,8 +134,8 @@ def blog_summit(request):
         file = request.FILES.get('first_image')
         title = request.POST.get('blog_title')
         title_converted = title.replace(' ', '-')
-        print(title_converted)
-        print(f'{title_converted}-{file}')
+        # print(title_converted)
+        # print(f'{title_converted}-{file}')
         storage.child(file).put(f'{title_converted}-{file}')
         url = storage.child(file).get_url(None)
         date = f'{datetime.datetime.now().strftime("%d")} ' \
