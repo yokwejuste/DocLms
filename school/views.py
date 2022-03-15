cad3433467340173540fa0a9529d07acc197c1b1
yokwejuste <yokwejuste@yahoo.com>
2022-03-15 02:43:33 +0100

    updates

--- school/views.py
+++ school/views.py
@@ -159,7 +159,6 @@ def blog_summit(request):
             "tags": blog_tags_set,
             "title": title,
         }
-        print(data)
         database.child('blog').child(title).set(data)
         return HttpResponse('success')
     return render(request, 'blog/blog-summit.html', context)
