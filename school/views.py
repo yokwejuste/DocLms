5aca1b09ff8a77593565d65a741e3854d65c07fb
yokwejuste <yokwejuste@yahoo.com>
2022-03-18 02:45:08 +0100

    updates

--- school/views.py
+++ school/views.py
@@ -16,42 +16,42 @@ def index(request):
         # 'framework': framework
         'home': 'active',
     }
-    return render(request, 'student/index.html', context)
+    return render(request, 'home/index.html', context)
 
 
 def index_2(request):
     context = {
         'home': 'active',
     }
-    return render(request, 'student/index-2.html', context)
+    return render(request, 'home/index-2.html', context)
 
 
 def index_3(request):
     context = {
         'home': 'active',
     }
-    return render(request, 'student/index-3.html', context)
+    return render(request, 'home/index-3.html', context)
 
 
 def index_4(request):
     context = {
         'home': 'active',
     }
-    return render(request, 'student/index-4.html', context)
+    return render(request, 'home/index-4.html', context)
 
 
 def about(request):
     context = {
         'about': 'active',
     }
-    return render(request, 'student/events.html', context)
+    return render(request, 'events/events.html', context)
 
 
 def events(request):
     context = {
         'events': 'active',
     }
-    return render(request, 'student/events.html', context)
+    return render(request, 'events/events.html', context)
 
 
 def blog(request):
@@ -84,28 +84,28 @@ def courses(request):
     context = {
         'courses': 'active',
     }
-    return render(request, 'student/courses.html', context)
+    return render(request, 'courses/courses.html', context)
 
 
 def contact(request):
     context = {
         'contact': 'active',
     }
-    return render(request, 'student/contact.html', context)
+    return render(request, 'contact/contact.html', context)
 
 
 def shop(request):
     context = {
         'shop': 'active',
     }
-    return render(request, 'student/shop.html', context)
+    return render(request, 'shop/shop.html', context)
 
 
 def single_shop(request):
     context = {
         'shop': 'active',
     }
-    return render(request, 'student/shop-single.html', context)
+    return render(request, 'shop/shop-single.html', context)
 
 
 def single_blog(request):
@@ -119,14 +119,14 @@ def single_course(request):
     context = {
         'course': 'active',
     }
-    return render(request, 'student/courses-single.html', context)
+    return render(request, 'courses/courses-single.html', context)
 
 
 def single_events(request):
     context = {
         'events': 'active',
     }
-    return render(request, 'student/events-single.html', context)
+    return render(request, 'events/events-single.html', context)
 
 
 def dashboard(request):
@@ -140,14 +140,14 @@ def teachers(request):
     context = {
         'teachers': 'active',
     }
-    return render(request, 'student/teachers.html', context)
+    return render(request, 'teachers/teachers.html', context)
 
 
 def teacher_single(request):
     context = {
         'teacher': 'active',
     }
-    return render(request, 'student/teachers-single.html', context)
+    return render(request, 'teachers/teachers-single.html', context)
 
 
 def login(request):
@@ -167,7 +167,7 @@ def login(request):
     if request.POST:
         auth.sign_in_with_email_and_password(email, password)
         redirect('s-dashboard')
-    return render(request, 'student/login.html', context)
+    return render(request, 'user_authentication/login.html', context)
 
 
 def register(request):
@@ -175,7 +175,7 @@ def register(request):
     password = request.POST.get('password')
     if request.POST:
         authed.create_user_with_email_and_password(email, password)
-    return render(request, 'student/register.html')
+    return render(request, 'user_authentication/register.html')
 
 
 def logout(request):
@@ -204,7 +204,7 @@ def blog_summit(request):
         for tag in blog__tags:
             arr.append(str(tag).capitalize())
         arr_2 = arr
-        blog_tags_set = mapping = dict(zip(arr, arr_2))
+        blog_tags_set = dict(zip(arr, arr_2))
         date = f'{datetime.datetime.now().strftime("%d")} ' \
                f'{datetime.datetime.now().strftime("%b")}' \
                f' {datetime.datetime.now().strftime("%Y")}'
