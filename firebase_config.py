3a1d68b0ebe690978ce382ba2d10fccf03484c33
yokwejuste <yokwejuste@yahoo.com>
2022-03-21 05:22:42 +0100

    update

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,14 @@
 import os
-from doc_lms.settings import BASE_DIR
+
 import firebase_admin
 from firebase_admin import credentials, firestore
+
+from doc_lms.settings import BASE_DIR
 
 cred = credentials.Certificate(os.path.join(BASE_DIR / 'serviceAccountKey.json'))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
+db.collection('users').add(
+    {'name': 'John Doe', 'age': 42, 'favorite_color': 'red', 'favorite_number': 7, 'favorite_food': 'pizza'})
