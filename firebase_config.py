f5816981ec009dfedbcc4e47a5b8016530d93151
yokwejuste <yokwejuste@yahoo.com>
2022-03-21 07:26:46 +0100

    update

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,9 @@
-import os
-
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-from doc_lms.settings import BASE_DIR
+from doc_lms.settings import firebase_admin_config_credentials
 
-cred = credentials.Certificate(os.path.join(BASE_DIR / 'serviceAccountKey.json'))
+cred = credentials.Certificate(firebase_admin_config_credentials)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
