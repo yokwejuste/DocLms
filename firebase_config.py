acc53b3eee4b311a820b94ae9d95dc232ff0d5e8
yokwejuste <yokwejuste@yahoo.com>
2022-03-21 09:10:17 +0100

    update

--- firebase_config.py
+++ firebase_config.py
@@ -1,9 +1,10 @@
 import firebase_admin
+import jsonpickle as jsonpickle
 from firebase_admin import credentials, firestore
 
 from doc_lms.settings import firebase_admin_config_credentials
 
-cred = credentials.Certificate(firebase_admin_config_credentials)
+cred = credentials.Certificate(jsonpickle.encode(firebase_admin_config_credentials))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
